d950516f528ef05b15aa70119972525d50e0a3ab
photoshovel <photoshovel@users.noreply.github.com>
2017-04-17 16:25:35 -0700

    PLAT-350 Modify new parameters to have default values rather than be named parameters

--- core/app/models/spree/promotion_builder.rb
+++ core/app/models/spree/promotion_builder.rb
@@ -18,7 +18,7 @@ class Spree::PromotionBuilder
   # @param promotion_attrs [Hash] The desired attributes for the newly promotion
   # @param attributes [Hash] The desired attributes for this builder
   # @param user [Spree::User] The user who triggered this promotion build
-  def initialize(attributes = {}, promotion_attributes = {}, promotion_rules: {}, promotion_actions: {})
+  def initialize(attributes = {}, promotion_attributes = {}, promotion_rules = {}, promotion_actions = {})
     @promotion = Spree::Promotion.new(promotion_attributes)
     super(attributes)
 
