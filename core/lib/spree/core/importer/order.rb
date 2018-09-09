bed8fee6bfaa14b21a87d82318acf33f4b5eb972
EC2 Default User <ec2-user@ip-172-31-1-20.us-west-2.compute.internal>
2018-09-09 21:24:48 +0000

    Troubleshooting Express Checkout: eating 'Price is not valid' error to try and get forensic evidence about what is different about Express Checkout orders vs Solidus web storefront.

--- core/lib/spree/core/importer/order.rb
+++ core/lib/spree/core/importer/order.rb
@@ -107,7 +107,7 @@ module Spree
               line_item = order.contents.add(Spree::Variant.find(line_item[:variant_id]), line_item[:quantity])
             rescue StandardError => e
                 Rails.logger.error("order: #{order.id} unable to add line item of variant_id: #{line_item[:variant_id]}, error message: #{e.message}, backtrace: #{e.backtrace.inspect}")
-                raise e
+                #raise e
             end
             # Looks like all details we care about are in the line_item object.
             # Perhaps we forget about the 'extra_params', since they are nil or 0 anyways?
