abe4519a6fa15f8ac838763aa3d13dc44626c01e
EC2 Default User <ec2-user@ip-172-31-1-20.us-west-2.compute.internal>
2018-11-01 22:12:56 +0000

    Fix to allow multiple Solidus stores with differing currencies.

--- api/app/controllers/spree/api/orders_controller.rb
+++ api/app/controllers/spree/api/orders_controller.rb
@@ -27,16 +27,14 @@ module Spree
 
       def create
         authorize! :create, Order
-
         if can?(:admin, Order)
           @order = Spree::Core::Importer::Order.import(determine_order_user, order_params)
-          @order.currency = 'CAD'
+          @order.currency = current_store.default_currency
           @order.save!
           respond_with(@order, default_template: :show, status: 201)
         else
-          # TODO: We need to un-hardcode 'currency' before supporting multiple countries
           @order = Spree::Order.create!(user: current_api_user, store: current_store)
-          @order.currency = 'CAD'
+          @order.currency = current_store.default_currency
           @order.save!
           if OrderUpdateAttributes.new(@order, order_params).apply
             respond_with(@order, default_template: :show, status: 201)
