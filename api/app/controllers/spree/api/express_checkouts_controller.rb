ae3f888e6b081b23883edcf83c65d6fdd1502889
photoshovel <eben@consultcalhoun.com>
2018-12-31 05:33:27 +0000

    PLAT-557 Yet another fix for $0.00 Express Checkout orders- this fix is for submit call.

--- api/app/controllers/spree/api/express_checkouts_controller.rb
+++ api/app/controllers/spree/api/express_checkouts_controller.rb
@@ -96,6 +96,7 @@ module Spree
 
           # Complete the payment.
           our_payment = @order.unprocessed_payments.last
+          unless our_payment.nil?
           # Validate that payment matches final order total- coupon(s) might 
           # have been applied, stealthily.
           #Rails.logger.debug("confirm payment: #{our_payment.inspect}")
@@ -103,27 +104,28 @@ module Spree
           #       minimum.  If not, set to 'External Payment'/check and log an error to 
           #       trigger an alert to Sales/Marketing that we are providing freebies.
           # TODO: Make this $1.00 setting configurable.
-          if @order.total <= 0.00 and our_payment.source_type != 
-            # TODO: Change payment method to check, if not already.
-            externally_paid_payment_method = Spree::PaymentMethod.find_by!(name: 'Externally Paid')
+            if @order.total <= 0.00 and our_payment.source_type != 
+              # TODO: Change payment method to check, if not already.
+              externally_paid_payment_method = Spree::PaymentMethod.find_by!(name: 'Externally Paid')
             
-            if our_payment.payment_method_id != externally_paid_payment_method.id
-              Rails.logger.debug("order # #{@order.id} switching to external payment (check) ")
-              our_payment.source_id = nil
-              our_payment.source_type = nil
-              our_payment.payment_method_id = externally_paid_payment_method.id
-              our_payment.amount = 0.0
-              our_payment.save!
-            end
+              if our_payment.payment_method_id != externally_paid_payment_method.id
+                Rails.logger.debug("order # #{@order.id} switching to external payment (check) ")
+                our_payment.source_id = nil
+                our_payment.source_type = nil
+                our_payment.payment_method_id = externally_paid_payment_method.id
+                our_payment.amount = 0.0
+                our_payment.save!
+              end
 
-          elsif our_payment.amount != @order.total
-            our_payment.amount = @order.total
-          end
+            elsif our_payment.amount != @order.total
+              our_payment.amount = @order.total
+            end
           
-          # This causes payment method(s) to run the charge.
-          our_payment.process!
-          # Checkout Payment Capture for 'check' payments and others that do not 'auto-capture'.
-          our_payment.capture!
+            # This causes payment method(s) to run the charge.
+            our_payment.process!
+            # Checkout Payment Capture for 'check' payments and others that do not 'auto-capture'.
+            our_payment.capture!
+          end
 
           # Proceed to Complete
           @order.complete!
